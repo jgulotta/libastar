0c2ee7f1c86b2cc2a7a6656ddb491e554a719296
Jacob Gulotta <jgulotta@gmail.com>
2015-07-15 21:47:22 -0700

    disable astar-algorithm's custom allocator (for now) to get a better baseline between standard library based implementations

--- examples/n-puzzle/src/astar-algorithm-cpp/stlastar.h
+++ examples/n-puzzle/src/astar-algorithm-cpp/stlastar.h
@@ -44,7 +44,7 @@ using namespace std;
 
 // Fixed size memory allocator can be disabled to compare performance
 // Uses std new and delete instead if you turn it off
-#define USE_FSA_MEMORY 1
+#define USE_FSA_MEMORY 0
 
 // disable warning that debugging information has lines that are truncated
 // occurs in stl headers
@@ -129,7 +129,11 @@ public: // methods
     {
     }
 
-    AStarSearch( int MaxNodes ) :
+    AStarSearch( int 
+#if USE_FSA_MEMORY
+            MaxNodes
+#endif
+            ) :
         m_State( SEARCH_STATE_NOT_INITIALISED ),
         m_CurrentSolutionNode( NULL ),
 #if USE_FSA_MEMORY
